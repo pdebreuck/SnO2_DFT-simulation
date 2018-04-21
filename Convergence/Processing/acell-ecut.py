f974799799a703763ddc682a76888ea8d0cba49a
pdebreuck <pierre-paul.debreuck@student.uclouvain.be>
2018-04-21 11:58:57 +0200

    p scripts

--- Convergence/Processing/acell-ecut.py
+++ Convergence/Processing/acell-ecut.py
@@ -37,15 +37,21 @@ print ccell
 print ecut
 acell =[x*9.11 for x in acell]
 ccell =[x*9.11 for x in ccell]
+
+aupper = [acell[-1]*(1+0.002) for x in acell]
+alower = [acell[-1]*(1-0.002) for x in acell]
+cupper = [ccell[-1]*(1+0.002) for x in ccell]
+clower = [ccell[-1]*(1-0.002) for x in ccell]
+
 plt.figure()
-plt.plot(ecut,acell,'o-',ecut,[acell[-1]*(1-0.002) for x in acell],'--',ecut,[acell[-1]*(1+0.002) for x in acell],'--')
-plt.xlabel('kinetic energy cutoff')
+plt.plot(ecut,acell,'o-',ecut,alower,'--',ecut,aupper,'--')
+plt.xlabel('Kinetic energy cutoff')
 plt.ylabel('Lattice parameter a')
 plt.title('acell - ecut convergence')
 plt.savefig('acell-ecut.png')
 
 plt.figure()
-plt.plot(ecut,ccell,'o-')
+plt.plot(ecut,ccell,'o-',ecut,clower,'--',ecut,c	upper,'--')
 plt.xlabel('Kinetic energy cutoff')
 plt.ylabel('Lattice parameter c')
 plt.title('ccell - ecut convergence')
